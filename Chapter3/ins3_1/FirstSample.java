15613cd364bca6ecd314097ffc75f05a6ebbbf31
DevinKin <DevinKin@163.com>
2018-01-04 14:09:40 +0800

    FirstSample

---
+++ Chapter3/ins3_1/FirstSample.java
@@ -0,0 +1,13 @@
+/*************************************************************************
+    @ File Name: FirstSample.java
+    @ Author: King
+    @ Mail: arturiapendragon_1@163.com 
+    @ Created Time: 2018年01月04日 星期四 14时08分00秒
+ ************************************************************************/
+public class FirstSample
+{
+	public static void main(String [] args)
+	{
+		System.out.println("We will not use 'Hello, World!'");
+	}
+}
