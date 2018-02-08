94bfebda05cb0c237feca2658a262a615e4f4673
DevinKin <DevinKin@163.com>
2018-02-08 22:55:15 +0800

    map

---
+++ Chapter13/map/src/map/Employee.java
@@ -0,0 +1,43 @@
+package map;
+import java.util.*;
+
+public class Employee {
+    public Employee(String n, double s, int year, int month, int day)
+    {
+        name = n;
+        salary = s;
+        GregorianCalendar calendar = new GregorianCalendar(year, month - 1, day);
+        //GregorianCalendar uses 0 for January
+        hireDay = calendar.getTime();
+    }
+
+    public Employee(String n)
+    {
+        name = n;
+    }
+
+    public String getName()
+    {
+        return name;
+    }
+
+    public double getSalary()
+    {
+        return salary;
+    }
+
+    public Date getHireDay()
+    {
+        return hireDay;
+    }
+
+    public void raiseSalary(double byPercent)
+    {
+        double raise = salary * byPercent / 100;
+        salary += raise;
+    }
+
+    private String name;
+    private double salary;
+    private Date hireDay;
+}
